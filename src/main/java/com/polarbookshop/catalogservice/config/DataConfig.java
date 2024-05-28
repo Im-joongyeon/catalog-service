9abb31ed80a4a92e164d4941f57c0a222bf654b9
Imgooltong <dlawnddus@naver.com>
2024-05-28 23:10:10 +0900

    add dependencies

---
+++ src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
@@ -0,0 +1,12 @@
+package com.polarbookshop.catalogservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+/**
+ * Created by I'm_joongyeon on 5/28/24.
+ */
+@Configuration
+@EnableJdbcAuditing
+public class DataConfig {
+}
